9b74840a089188a184c2f6ab02193d785df50df6
Koumba Thiongane <koumbathiongane201@gmail.com>
2025-04-10 21:46:25 +0200

    commit one

---
+++ src/main/java/sn/uasz/ParametresAPI/ParametresApiApplication.java
@@ -0,0 +1,13 @@
+package sn.uasz.ParametresAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParametresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParametresApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/uasz/ParametresAPI/ParametresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.ParametresAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParametresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
